97a81be93330024564f18c584dd83940fecb4ea9
MJstephenson <>
2023-06-24 14:48:07 +0000

    Add register functionality to form

--- app.py
+++ app.py
@@ -28,6 +28,24 @@ def get_add_game():
 
 @app.route("/register", methods=["GET", "POST"])
 def register():
+    if request.method == "POST":
+        # check if username already exists in db
+        existing_user = mongo.db.users.find_one(
+            {"username": request.form.get("username").lower()})
+
+        if existing_user:
+            flash("Username already exists")
+            return redirect(url_for("register"))
+
+        register = {
+            "username": request.form.get("username").lower(),
+            "password": generate_password_hash(request.form.get("password"))
+        }
+        mongo.db.users.insert_one(register)
+
+        # put the new user into 'session' cookie
+        session["user"] = request.form.get("username").lower()
+        flash("Your Registration Was Successful!")
     return render_template("register.html")
 
 
@@ -35,5 +53,4 @@ if __name__ == "__main__":
     app.run(host=os.environ.get("IP"),
             port=int(os.environ.get("PORT")),
             debug=True)
-
-
+            
