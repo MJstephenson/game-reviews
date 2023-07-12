b5e07629b3ca5eb80533b8a78f9076ae8953c3f5
MJstephenson <cyclomark@gmail.com>
2023-07-12 21:02:19 +0000

    Remove console.log for delete  review dialog box

--- static/js/script.js
+++ static/js/script.js
@@ -10,7 +10,6 @@ window.onload = function () {
     function confirmDelete (game_name, formId) { // get the game_name & formId parameters
         // show a confirm delete modal to the user to confirm deletion of their review
         const deleteReview = confirm("Are you sure you would like to delete your " + game_name + " review?")
-        console.log("Submitting form with ID:", formId);
         if (deleteReview == true) {
             // grab the form id and call the submit from the form to delete the review
             document.getElementById(formId).submit();
