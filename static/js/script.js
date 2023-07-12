1dd6fd0eed4523a964da92f83e8ada608e61f6a3
MJstephenson <cyclomark@gmail.com>
2023-07-12 21:00:13 +0000

    Add code  for delete confirmation dialog box

--- static/js/script.js
+++ static/js/script.js
@@ -6,7 +6,13 @@ window.onload = function () {
         min: new Date(2020, 5, 10),
         max: new Date(2023, 5, 20)
     });
-
-    
 }
-
+    function confirmDelete (game_name, formId) { // get the game_name & formId parameters
+        // show a confirm delete modal to the user to confirm deletion of their review
+        const deleteReview = confirm("Are you sure you would like to delete your " + game_name + " review?")
+        console.log("Submitting form with ID:", formId);
+        if (deleteReview == true) {
+            // grab the form id and call the submit from the form to delete the review
+            document.getElementById(formId).submit();
+        }
+    }
