e8cabf4f64dbc6146554dcb9b066259781e36466
MJstephenson <cyclomark@gmail.com>
2023-07-16 20:06:16 +0000

    Add review_count for user_id to show review number

--- app.py
+++ app.py
@@ -104,9 +104,11 @@ def profile(username):
     # this will get the users username from the database
     username = mongo.db.users.find_one(
         {"username":session["user"]})["username"]
+    # see how many reviews the user has in my database
+    review_count = mongo.db.add_review.count_documents({"user_id": ObjectId(session['user_id'])})
 
     if session['user']:
-        return render_template("profile.html", username=username)
+        return render_template("profile.html", username=username, review_count=review_count)
 
     return redirect(url_for("login"))
 
@@ -196,8 +198,6 @@ def edit_review(review_id):
         # Gets the requirde review to edit from my database
         review_to_edit = mongo.db.add_review.find_one({'_id': ObjectId(review_id)})
         return render_template('edit_review.html', review=review_to_edit)
-
-
 
 
 def get_games():
