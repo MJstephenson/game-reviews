9b457578d78faf0791433221b10b358f3ce557ab
MJstephenson <cyclomark@gmail.com>
2023-08-05 10:52:46 +0000

    Remove tailwind datepicker  to fix console error

--- static/js/script.js
+++ static/js/script.js
@@ -1,11 +1,5 @@
 window.onload = function () {
 
-    const datepickerWithLimits = document.getElementById('datepicker-with-limits');
-    new Datepicker(datepickerWithLimits, {
-        min: new Date(2020, 5, 10),
-        max: new Date(2023, 5, 20)
-    });
-}
     function confirmDelete (game_name, formId) { // get the game_name & formId parameters
         // show a confirm delete modal to the user to confirm deletion of their review
         const deleteReview = confirm("Are you sure you would like to delete your " + game_name + " review?")
@@ -14,3 +8,4 @@ window.onload = function () {
             document.getElementById(formId).submit();
         }
     }
+}
